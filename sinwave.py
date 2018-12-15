1fc10d656bfd69baff3fcfe3e95c4016a3eef476
Juha Kiili <juha@juhakiili.com>
2018-12-15 14:01:51 +0200

    testing...

--- sinwave.py
+++ sinwave.py
@@ -3,6 +3,7 @@ import argparse
 import json
 import tensorflow as tf
 import sys
+import time
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -12,13 +13,16 @@ if __name__ == '__main__':
   parser.add_argument('--sinscale', type=float, default=1.0)
   parser.add_argument('--sinfreq2', type=float, default=1.0)
   parser.add_argument('--sinscale2', type=float, default=1.0)
+  parser.add_argument('--stepdelay', type=float, default=0.01)
   FLAGS, unparsed = parser.parse_known_args()
   for i in range(0, 100):
     print( \
       json.dumps({ \
         'step': i, \
-        'sqrstep': math.sqrt(i), \
+        'sqrstep': math.sqrt(i*FLAGS.sinscale), \
+        'sqrstep': math.sqrt(i*FLAGS.sinscale2), \
         'sinwave': FLAGS.sinscale * math.sin(i * FLAGS.sinfreq), \
         'sinwave2': FLAGS.sinscale2 * math.sin(i * FLAGS.sinfreq2) \
         }))
+    time.sleep(FLAGS.stepdelay)
 
