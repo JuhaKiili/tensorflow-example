d0733d011fad60eb84df5dc7cdce1d23588bb8d7
Juha Kiili <juha@juhakiili.com>
2018-12-15 13:55:21 +0200

    testing...

--- sinwave.py
+++ sinwave.py
@@ -4,6 +4,16 @@ import json
 import tensorflow as tf
 import sys
 
+def main(_):
+  for i in range(0, 100):
+    print( \
+      json.dumps({ \
+        'step': i, \
+        'sqrstep': math.sqrt(i), \
+        'sinwave': FLAGS.sinscale * math.sin(i * FLAGS.sinfreq), \
+        'sinwave2': FLAGS.sinscale2 * math.sin(i * FLAGS.sinfreq2) \
+        }))
+        
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--max_steps', type=int, default=300,
@@ -14,13 +24,3 @@ if __name__ == '__main__':
   parser.add_argument('--sinscale2', type=float, default=1.0)
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
-
-def main(_):
-  for i in range(0, 100):
-    print( \
-      json.dumps({ \
-        'step': i, \
-        'sqrstep': math.sqrt(i), \
-        'sinwave': FLAGS.sinscale * math.sin(i * FLAGS.sinfreq), \
-        'sinwave2': FLAGS.sinscale2 * math.sin(i * FLAGS.sinfreq2) \
-        }))
