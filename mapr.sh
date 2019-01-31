f08dd2e023c8c8bf52f60e7d2208c3929e7851cb
Juha Kiili <juha@juhakiili.com>
2019-01-31 14:18:59 +0200

    Echo version

--- mapr.sh
+++ mapr.sh
@@ -4,14 +4,18 @@ if [ -z "$MAPR_SNAPSHOT" ]
 then
   $MAPR_SNAPSHOT=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
   echo "Creating MapR snapshot: $MAPR_SNAPSHOT"
-  curl -k -X POST "https://$MAPR_REST/rest/volume/snapshot/create?volume=$MAPR_VOLUME&snapshotname=$MAPR_SNAPSHOT" --user valohai:valohai
+  echo curl -k -X POST "https://$MAPR_REST/rest/volume/snapshot/create?volume=$MAPR_VOLUME&snapshotname=$MAPR_SNAPSHOT" --user valohai:valohai
+  # curl -k -X POST "https://$MAPR_REST/rest/volume/snapshot/create?volume=$MAPR_VOLUME&snapshotname=$MAPR_SNAPSHOT" --user valohai:valohai
 else
   echo "Using existing MapR snapshot: $MAPR_SNAPSHOT"
 fi
 
-wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/training-set-images/train-images-idx3-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/train-images-idx3-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
-wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/training-set-labels/train-labels-idx1-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/train-labels-idx1-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
-wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/test-set-images/t10k-images-idx3-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/t10k-images-idx3-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
-wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/test-set-labels/t10k-labels-idx1-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/t10k-labels-idx1-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
+echo wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/training-set-images/train-images-idx3-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/train-images-idx3-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
+echo wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/training-set-labels/train-labels-idx1-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/train-labels-idx1-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
+echo wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/test-set-images/t10k-images-idx3-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/t10k-images-idx3-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
+echo wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/test-set-labels/t10k-labels-idx1-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/t10k-labels-idx1-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
 
-python train.py {parameters}
+# wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/training-set-images/train-images-idx3-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/train-images-idx3-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
+# wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/training-set-labels/train-labels-idx1-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/train-labels-idx1-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
+# wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/test-set-images/t10k-images-idx3-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/t10k-images-idx3-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
+# wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/test-set-labels/t10k-labels-idx1-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/t10k-labels-idx1-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
