901387a2bc299b0037d253a444d1b252183e9054
Juha Kiili <juha@juhakiili.com>
2019-02-01 10:30:33 +0200

    Fix yaml

--- mapr.sh
+++ mapr.sh
@@ -9,8 +9,8 @@ else
   echo "Using existing MapR snapshot: $MAPR_SNAPSHOT"
 fi
 
-wget -k --no-verbose "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/training-set-images/train-images-idx3-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/training-set-images/train-images-idx3-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
-wget -k --no-verbose "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/training-set-labels/train-labels-idx1-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/training-set-labels/train-labels-idx1-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
-wget -k --no-verbose "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/test-set-images/t10k-images-idx3-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/test-set-images/t10k-images-idx3-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
-wget -k --no-verbose "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/test-set-labels/t10k-labels-idx1-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/test-set-labels/t10k-labels-idx1-ubyte.gz" --user $MAPR_USERNAME --password $MAPR_PASSWORD
+wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/training-set-images/train-images-idx3-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/training-set-images/train-images-idx3-ubyte.gz" --verbose --user $MAPR_USERNAME --password $MAPR_PASSWORD
+wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/training-set-labels/train-labels-idx1-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/training-set-labels/train-labels-idx1-ubyte.gz" --verbose --user $MAPR_USERNAME --password $MAPR_PASSWORD
+wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/test-set-images/t10k-images-idx3-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/test-set-images/t10k-images-idx3-ubyte.gz" --verbose --user $MAPR_USERNAME --password $MAPR_PASSWORD
+wget -k "http://$MAPR_HTTPFS/webhdfs/v1/tmp/$MAPR_VOLUME/.snapshot/$MAPR_SNAPSHOT/test-set-labels/t10k-labels-idx1-ubyte.gz?op=open" -O "$VH_INPUTS_DIR/test-set-labels/t10k-labels-idx1-ubyte.gz" --verbose --user $MAPR_USERNAME --password $MAPR_PASSWORD
 ls -lR /valohai/inputs
